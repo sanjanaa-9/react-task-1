4c24fce60aa39c97fd5d336130eef951d44b5183
unknown <sanjnameena74@gmail.com>
2025-02-21 15:04:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-black text-3xl text-white'>
+          <h1>hello its working</h1>
+    </div>
+  )
+}
+
+export default App
